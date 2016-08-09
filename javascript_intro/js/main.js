2401437499b2d615c2e613a5cfee8579a7543665
jaenycat22 <jae.k.cheong@gmail.com>
2016-08-09 17:50:16 -0400

    day6

---
+++ javascript_intro/js/main.js
@@ -0,0 +1,38 @@
+
+// //initializing a variable
+// var score_counter;
+
+// //assign a value to a variable
+// score_counter = 10;
+
+// //display my score
+
+// console.log("the score is "+ score_counter);
+// alert("the score is "+ score_counter);
+
+//declare and initialize 3 different variables
+//print all of them to the console
+//test out comparison operators in one variable 
+
+// var student_num;
+// var teacher_num;
+// var book_num;
+
+// student_num = 10;
+// teacher_num = 2;
+// book_num = 15;
+
+// console.log("In our school, we have " + student_num + "student," + teacher_num + "teacher and" + book_num + "books");
+
+// var user_answer = prompt("what is your name?");
+// var password = prompt("what is your password?");
+// console.log("hello" + user_answer);
+// console.log("your password is " + password);
+
+document.getElementById("main_paragraph").style.color="pink";
+document.getElementById("second_paragraph").style.color="blue";
+document.getElementById("third_paragraph").style.color="green";
+
+// document.getElementsByClassName("body_text")[0].style.display="none";
+
+document.getElementById("pop_up").style.display="none";
